5db2fafef2f1c455754ed79eea241333af76fdc5
Mishra swapnil <sammishra644@gmail.com>
2022-01-25 21:47:22 +0530

    updated css sheet
    added table border
    made some adjustment
    edited js file

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const clickButton = document.querySelector("#click-btn");
 const numberOfNotes = document.querySelectorAll(".no-of-notes");
+const errorMessages = document.querySelector("#error-message");
+
+const availableNotes = [2000,500,200,100,50,20,10,5,2,1];
+
+
