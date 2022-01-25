f0f2f88336cf4c89b04da7c16460816443c68988
Mishra swapnil <sammishra644@gmail.com>
2022-01-25 11:57:51 +0530

    created basic structure
    created html,css and js file and connected them together and build the basic structure for further functions.

---
+++ app.js
@@ -0,0 +1,4 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const clickButton = document.querySelector("#click-btn");
+const numberOfNotes = document.querySelectorAll(".no-of-notes");
