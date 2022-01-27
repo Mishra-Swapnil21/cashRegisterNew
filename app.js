922ea610f139403f67cf782fec5e68e726265ee8
Mishra swapnil <sammishra644@gmail.com>
2022-01-27 14:09:22 +0530

    corrected error,added footer
    corrected error occuring while inputting 21-100 integer number
    added footer to the webpage

--- app.js
+++ app.js
@@ -6,42 +6,40 @@ const errorMessage = document.querySelector("#error-message");
 
 const availableNotes = [2000,500,200,100,50,20,10,5,2,1];
 
+hideMessage()
 
+clickButton.addEventListener('click', function validateBillAndCashAmount()
+{hideMessage();
+    const billAmt = Number(billAmount.value);
+   const cashGvn  = Number(cashGiven.value);
+ if(billAmt>0)
+ {
+     if(cashGvn > 0)
+     {
+      if(cashGvn>=billAmt)
+      {
+      const amountToBeReturned = cashGvn - billAmt;
+      calculateChange(amountToBeReturned);    
 
-clickButton.addEventListener("click" , function validateBillAndCashAmount() {
-    if(billAmount.value > 0){
-        hideMessage();
-        if(cashGiven.value>=billAmount.value){
-            const AmountToBeReturned = cashGiven.value - billAmount.value;
-            calculateChange(AmountToBeReturned)
-            
-        }
-        else {
-            showMessage("Do you want to wash dishes ?");
-        }
-    }
-    else {
-        showMessage("error:- Invalid Bill Amount");
-    }
-});   
-
-
+      }else{showMessage("Do you want to wash dishes ?")}   
+     }else{showMessage("error:- Invalid Cash Given Amount.")}
+ }else { showMessage("error:- Invalid Bill Amount.")}
+});
 
 function hideMessage(){
     errorMessage.style.display = "none";
 }
 
-function calculateChange(amountToBereturned){
+function calculateChange(amountToBeReturned){
     for (let i = 0 ; i <availableNotes.length;i++)
     {
-        const noOfNotes = Math.trunc(amountToBereturned/availableNotes[i]);
-        amountToBereturned = amountToBereturned % availableNotes[i];
+        const noOfNotes = Math.trunc(amountToBeReturned/availableNotes[i]);
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
         numberOfNotes[i].innerText = noOfNotes ; 
     }
 }
 
-
 function showMessage(msg){
     errorMessage.style.display = "block";
     errorMessage.innerText = msg;
-}
+ }
